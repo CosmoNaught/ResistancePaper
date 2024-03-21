5280d0b040f10464e20c15128831af5b915ea9f5
CosmoNaught <santonicosmo@gmail.com>
2024-03-21 10:22:24 +0000

    Update main.R

--- scripts/post/main.R
+++ scripts/post/main.R
@@ -9,13 +9,13 @@ source(paste0(getwd(), "/scripts/utils/utils.R"))
 debug <- FALSE # Toggle based on requirement
 environment_label <- ifelse(debug, "debug", "final")
 measure_type <- "prevalence"  # Can be set to "incidence" or "prevalence"
-modes <- c("observed", "PyOnly", "PyPBO", "IG2Only", "counterfactual")
+modes <- "observed" #c("observed", "PyOnly", "PyPBO", "IG2Only", "counterfactual")
 
 iso_codes <- unique(read.csv("D:/Malaria/ResistancePaper/data/post/SSA_region_combined.csv")$ISO3C)
 check_iso_codes(iso_codes)
 iso_codes <- iso_codes[iso_codes != "CPV"]
 
-iso_codes <- iso_codes
+iso_codes <- "MLI" #iso_codes
 
 # Adjusted Directory Paths Function
 get_directory_paths <- function(environment_label, mode) {
@@ -24,43 +24,43 @@ get_directory_paths <- function(environment_label, mode) {
   list(base_dir = base_dir, post_dir_base = post_dir_base)
 }
 
-  # Utility Functions
-  preprocess_data <- function(file_path, source_label, measure_type) {
-    data <- readRDS(file_path)
+# Utility Functions
+preprocess_data <- function(file_path, source_label, measure_type) {
+  data <- readRDS(file_path)
+  data <- data %>%
+    mutate(timestep = floor((timestep / 365) + 2000),  # Convert days to whole years 
+          net_types = source_label)
+  browser()
+  if (measure_type == "incidence") {
     data <- data %>%
-      mutate(timestep = floor((timestep / 365) + 2000),  # Convert days to whole years 
-            net_types = source_label)
-
-    if (measure_type == "incidence") {
-      data <- data %>%
-        select(timestep, net_types, n_inc_clinical_1825_5474) %>%
-        mutate(value = n_inc_clinical_1825_5474 / n_1825_5474) # number of new clinical cases for each timestep for the age range not actual incidence -- to get clinical incidence divide by number of individuals in that age range
-    } else if (measure_type == "prevalence") {
-      data <- data %>%
-        select(timestep, net_types, n_detect_730_3649, n_730_3649) %>%
-        mutate(value = n_detect_730_3649 / n_730_3649)
-    } else if (measure_type == "cases") {
-      data <- data %>%
-        select(timestep, net_types, n_detect_730_3649, n_730_3649) %>%
-        mutate(value = n_infections) ## number of cases in every timestep annually sum them (but convert from sim pop to actual pop so divide by sim pop * actual pop)
-      }
-
-    return(data)
-  }
-
-  aggregate_data <- function(data, measure_type) {
-    if (measure_type == "incidence") {
-      data %>%
-        group_by(timestep, net_types) %>%
-        summarise(value = mean(value, na.rm = TRUE)) %>%
-        ungroup()
-    } else if (measure_type == "prevalence") {
-      data %>%
-        group_by(timestep, net_types) %>%
-        summarise(value = mean(value, na.rm = TRUE)) %>%
-        ungroup()
+      select(timestep, net_types, n_inc_clinical_1825_5474) %>%
+      mutate(value = n_inc_clinical_1825_5474 / n_1825_5474) # number of new clinical cases for each timestep for the age range not actual incidence -- to get clinical incidence divide by number of individuals in that age range
+  } else if (measure_type == "prevalence") {
+    data <- data %>%
+      select(timestep, net_types, n_detect_730_3649, n_730_3649) %>%
+      mutate(value = n_detect_730_3649 / n_730_3649)
+  } else if (measure_type == "cases") {
+    data <- data %>%
+      select(timestep, net_types, n_detect_730_3649, n_730_3649) %>%
+      mutate(value = (n_infections)) ## number of cases in every timestep annually sum them (but convert from sim pop to actual pop so divide by sim pop * actual pop)
     }
+
+  return(data)
+}
+
+aggregate_data <- function(data, measure_type) {
+  if (measure_type == "incidence") {
+    data %>%
+      group_by(timestep, net_types) %>%
+      summarise(value = mean(value, na.rm = TRUE)) %>%
+      ungroup()
+  } else if (measure_type == "prevalence") {
+    data %>%
+      group_by(timestep, net_types) %>%
+      summarise(value = mean(value, na.rm = TRUE)) %>%
+      ungroup()
   }
+}
 
 # Main Execution Logic Adjusted for New Modes and ISOs
 process_and_combine_data_for_mode_and_iso <- function(mode, iso, measure_type) {
@@ -88,3 +88,42 @@ for (iso in iso_codes) {
     process_and_combine_data_for_mode_and_iso(mode, iso, measure_type)
   }
 }
+
+
+#######################
+
+# per person for a given time-span
+
+# clinical incidence between 2 time frames (start and end year) per person for each time point, then sum for the year 
+
+# s_date start of sim at 2018
+# s_end end of sim at 2022
+
+# so select rows of n_inc_clinical_
+
+# index start date of 2019 to end of 2022 and
+
+calc_incidence <- function(output, s_date, e_date, min_age, max_age){
+  s_time <- which(output$date == s_date) 
+  e_time <- which(output$date == e_date)
+  return(output[s_time:e_time,paste0("n_inc_clinical_",min_age,"_",max_age)]/
+  output[s_time:e_time, paste0("n_", min_age,"_", max_age)])
+}
+
+# incidence
+# multiply by however many cases 
+calc_incidence <- function(output, s_index, e_index, min_age, max_age){
+  sum(output[s_index:e_index,paste0("n_inc_clinical_",min_age,"_",max_age)]/ 
+  output[s_index:e_index, paste0("n_", min_age,"_", max_age)])
+}
+
+data <- readRDS("D:/Malaria/ResistancePaper/outputs/raw/sim/final/counterfactual/AGO/pre_model_output_Bengo_rural.RDS")
+
+# cases per person for the 3 year period 2019 start to end 2022
+# first day of 2019, to last day of 2022, for the 0 - 5 age range
+# number of cases per person over the 3 year for that given region
+calc_incidence(data, 365*19, 365*23 - 1, 0, 1824) * 100000
+
+data$n_0_1824
+
+## take average for the 3 year range from data$n_0_1825
